a5a7ae871beadad1c47c2e464f113dbaa4c95518
ricardo123nac <esteban1525marti@gmail.com>
2024-02-08 18:55:32 -0500

    Guardado 08/02/2024

---
+++ Holamundo.ts
@@ -0,0 +1,81 @@
+
+// // import * as  readline from "readline"
+// console.log("Hola mundo")
+// let n1: number = 12
+// let n2: number = 10
+
+
+// console.log(n1 + n2)
+
+
+// console.log("PROMEDIO")
+
+// let booleano: boolean = true
+// console.log(`Booleanos: ${booleano}`)
+
+// let string: string = "Cadena de caracteres"
+// console.log(`String: ${string}`)
+
+// let flotante: number = 2.04
+// console.log("Flotante: " + flotante)
+
+
+
+// console.log("CONDICIONAL ")
+// const numero: number=2
+// if (numero >= 18) {
+//     console.log("Es mayor")
+// } else { 
+//     console.log("Es menor")
+// }
+
+// console.log("ARRAY")
+// let array: number[] = [
+//     1,2,3,4,5,6,7,8,9,10,11,15
+// ]
+
+// let buscarnumber: number = 10
+// let todoarray = array.map(function (element) { 
+    
+//     if (buscarnumber == element) {
+//       console.log(buscarnumber)
+//     } else {
+//         console.log("El numero que ingreso no existe en el ARRAY")
+//     }
+
+//     return element
+// })
+// console.log(" ARRAY COMPLETO: " + todoarray)
+
+// console.log("CLASES")
+// class Computier {
+//     numeroderef: number
+//     modelo: string
+//     color: string
+//     // año:[]     
+
+//     constructor(numeroderef: number, modelo: string, color: string) { 
+//         this.numeroderef = numeroderef
+//         this.modelo = modelo
+//         this.color= color
+//     }
+
+//     getNumeroderef() { 
+//         return this.numeroderef
+//     }
+//     setNumeroderef(numerito:number) { 
+//         this.numeroderef = numerito
+//     }
+//     encenderComputier() { 
+//         return "El computador esta encendido " + this.numeroderef
+//     }
+// }
+// let Computador = new Computier(10211,"Asus","Morado")
+// Computador.encenderComputier()
+// console.log(Computador.encenderComputier())
+
+// Computador.setNumeroderef(12445)
+// console.log(Computador.encenderComputier())
+// console.log("NUMERO DE REFERENCIA: " + Computador.getNumeroderef())
+
+// const teclado = readline 
---
+++ ejercicio.ts
@@ -0,0 +1,81 @@
+class Persona {
+    nombre: String
+    edad: number
+    telefono: number
+    direccion: string
+    ciudad: string
+
+    constructor(nombre: string, edad: number, telefono: number, direccion: string, ciudad: string) {
+        this.nombre = nombre
+        this.edad = edad
+        this.telefono = telefono
+        this.direccion = direccion
+        this.ciudad = ciudad
+    }
+
+    //Get
+    getNombre() {
+        return this.nombre
+    }
+    getEdad() {
+        return this.edad
+    }
+    getTelefono() {
+        return this.telefono
+    }
+    getDireccion() {
+        return this.direccion
+    }
+    getCiudad() {
+        return this.ciudad
+    }
+    //set
+    setNombre(name: string) {
+        this.nombre = name
+    }
+    setEdad(edad: number) {
+        this.edad = edad
+    }
+    setTelefono(telefo: number) {
+        this.telefono = telefo
+    }
+    setDireccion(direccion: string) {
+        this.direccion = direccion
+    }
+    setCiudad(city: string) {
+        return this.ciudad = city
+    }
+
+    //METODOS
+
+}
+function mostrarDatos(personas: Persona[]) {
+    for (let i = 0; i < personas.length; i++) {
+        console.log(personas[i].nombre, personas[i].edad, personas[i].telefono, personas[i].direccion, personas[i].ciudad)
+    }
+}
+function mayorOmenor(personass: Persona[]) {
+    for (let i = 0; i < personass.length; i++) {
+        if (personas[i].edad > 18) {
+            console.log("ES MAYOR"+personass[i].edad,personass[i].nombre)
+        } else {
+            console.log("Es MENOR de edad")
+        }
+    }
+}
+let persona1 = new Persona("Manuel", 18, 6975754, "Calle 26N 58", "Popayan")
+let persona2 = new Persona("Maryuri", 18, 94667796, "Calle 26N 5242", "Bolivar Cauca")
+let persona3 = new Persona("Carlos", 22, 54601575754, "carrera 10 #56", "Popayan")
+let persona4 = new Persona("Miranda", 30, 976468754, "Calle 26N 60", "Cali")
+let persona5 = new Persona("Nicolas", 45, 97531046, "Calle 27N 30", "Popayan")
+
+let personas: Persona[] = [persona1, persona2, persona3, persona4, persona5]
+let personass: Persona[] = [persona1, persona2, persona3, persona4, persona5]
+
+
+console.log("METODOS")
+console.log("MOSTRAR DATOS")
+console.log("Todas las personas")
+console.log(mostrarDatos(personas))
+console.log(mayorOmenor(personas))
+// console.log(` Datos de la perosna ${persona1.getNombre() , persona1.getEdad(),persona1.getTelefono
