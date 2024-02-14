17192206a358605c6e7b809f7ea3c08d166c5cbd
ricardo123nac <esteban1525marti@gmail.com>
2024-02-14 18:38:40 -0500

    cambio

--- Cuenta.ts
+++ Cuenta.ts
@@ -24,3 +24,33 @@ class Cuenta {
         return this.valorActual = valu
     }
 }
+
+
+function muestraDatos(datos: Cuenta[]) { 
+    for (let i = 0; i < datos.length; i++) {
+        console.log(`Datos: ${datos[i].titular} ${datos[i].valorActual}`)
+     }
+}
+function Ingresar(datos: Cuenta[]) { 
+    for (let i = 0; i < datos.length; i++) { 
+        if (datos[i].valorActual < 0) {
+            console.log("No es VALIDO")
+        } else { 
+           console.log(`SUMA DE DATOS : ${datos[i].valorActual + datos[i].valorActual}`) 
+        }
+    }
+
+}
+function Retirar(datos: Cuenta[]) {
+    
+}
+
+let Persona1 = new Cuenta("Maryuri Lopez", -565)
+let Persona2 = new Cuenta("Angie Lopez", 8.000)
+let datos: Cuenta[] = [Persona1, Persona2]
+
+console.log("MUESTRA DE DATOS")
+console.log(muestraDatos(datos))
+
+console.log(Ingresar(datos))
+
